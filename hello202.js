2fe0b7a9f8e4ade6b0a051833e52561137b51910
bahngdaheun <reparo@naver.com>
2021-06-09 16:36:30 +0900

    수정

--- hello202.js
+++ hello202.js
@@ -1,3 +1,4 @@
 console.log("타자가");
 console.log("빠르시네요.");
 console.log("..");
+console.log("감사합니다~~");
--- hello204.js
+++ hello204.js
@@ -1,3 +1,4 @@
+console.log("123456")
 console.log("gg");
-console.log("33");
+console.log("33====>jkl");
 console.log("44");
